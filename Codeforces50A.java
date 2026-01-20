bf67b20220ec1837583f3ce966a5e4ee5efac0b6
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-21 01:17:34 +0530

    Create Codeforces50A.java

---
+++ Codeforces50A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Codeforces50A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int maxDominoes = (m * n) / 2;
+        System.out.println(maxDominoes);
+    }
+}
