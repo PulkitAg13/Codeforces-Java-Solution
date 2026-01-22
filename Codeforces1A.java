42b9a1bc94c7bc119f13c38a1c788b28172314a5
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-22 21:09:24 +0530

    Update Codeforces1A.java

--- Codeforces1A.java
+++ Codeforces1A.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Codeforces1A {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long a = sc.nextLong();
+
+        long x = (n + a - 1) / a;
+        long y = (m + a - 1) / a;
+
+        long ans = x * y;
+        System.out.println(ans);
+    }
+}
+
 }
