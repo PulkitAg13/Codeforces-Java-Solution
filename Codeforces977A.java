61f8f9f25fc902f8ca33dd272d577561698ca40a
PulkitAg13 <pulkit.ag13@gmail.com>
2026-01-31 00:05:21 +0530

    Create Codeforces977A.java

---
+++ Codeforces977A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Codeforces977A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        int k = sc.nextInt();
+
+        for (int i = 0; i < k; i++) {
+            if (n % 10 != 0) {
+                n = n - 1;
+            } else {
+                n = n / 10;
+            }
+        }
+
+        System.out.println(n);
+    }
+}
