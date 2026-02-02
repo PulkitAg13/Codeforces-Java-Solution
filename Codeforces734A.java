b1a2e7228f0b480841c350f85c44f8c80b66c434
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-03 00:33:03 +0530

    Update Codeforces734A.java

--- Codeforces734A.java
+++ Codeforces734A.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;           
 
-public class Codeforces732A {
+public class Codeforces734A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
