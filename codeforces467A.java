e9d3da8ade261ac7e966533294601a485d78a9b0
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-06 00:34:08 +0530

    Create codeforces467A.java

---
+++ codeforces467A.java
@@ -0,0 +1,17 @@
+public class codeforces467A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            if (q - p >= 2) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
