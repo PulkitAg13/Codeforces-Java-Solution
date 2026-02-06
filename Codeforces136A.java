75230d29ef2c2ec6ff7ff7dd766736283d18fb0d
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-07 00:26:11 +0530

    Create Codeforces136A.java

---
+++ Codeforces136A.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Codeforces136A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] p = new int[n];
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            p[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans[p[i] - 1] = i + 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
