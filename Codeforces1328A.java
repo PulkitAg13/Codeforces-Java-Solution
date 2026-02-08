f61d45f531dc9db736a0335c00fcfd9ec0ddf5c5
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-09 00:26:16 +0530

    Create Codeforces1328A.java

---
+++ Codeforces1328A.java
@@ -0,0 +1,17 @@
+public class Codeforces1328A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+
+            long rem = a % b;
+            long ans = (b - rem) % b;
+
+            System.out.println(ans);
+        }
+    }
+}
