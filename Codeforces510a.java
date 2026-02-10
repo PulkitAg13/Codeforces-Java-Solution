365fa0cbefe4bd7e50f2c7e346c60e606715f095
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-11 00:22:03 +0530

    Create Codeforces510a.java

---
+++ Codeforces510a.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Codeforces510a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                for (int j = 0; j < m; j++) System.out.print("#");
+            } else {
+                if ((i / 2) % 2 == 1) {
+                    for (int j = 0; j < m - 1; j++) System.out.print(".");
+                    System.out.print("#");
+                } else {
+                    System.out.print("#");
+                    for (int j = 0; j < m - 1; j++) System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
