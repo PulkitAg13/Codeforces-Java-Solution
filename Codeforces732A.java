3855d84a02260f579bb6a9ed4283653e310350cd
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-16 00:09:38 +0530

    Create Codeforces732A.java

---
+++ Codeforces732A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Codeforces732A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int r = sc.nextInt();
+
+        int i = 1;
+        while (true) {
+            int total = k * i;
+            if (total % 10 == 0 || total % 10 == r) {
+                System.out.println(i);
+                break;
+            }
+            i++;
+        }
+    }
+}
