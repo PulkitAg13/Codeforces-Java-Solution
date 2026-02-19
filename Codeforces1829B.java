ac8eb40ebfe3b6046fd279d4188e19a1603c9268
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-20 00:30:43 +0530

    Create Codeforces1829B.java

---
+++ Codeforces1829B.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Codeforces1829B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            
+            int maxLen = 0;
+            int cur = 0;
+
+            for(int i = 0; i < n; i++) {
+                int x = sc.nextInt();
+                
+                if(x == 0) {
+                    cur++;
+                    if(cur > maxLen) {
+                        maxLen = cur;
+                    }
+                } else {
+                    cur = 0;
+                }
+            }
+
+            System.out.println(maxLen);
+        }
+    }
+}
