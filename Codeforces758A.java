c12879840c68593b3606442f936027462489f0ca
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-21 00:14:33 +0530

    Create Codeforces758A.java

---
+++ Codeforces758A.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Codeforces758A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        int max = 0;
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        long ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            ans += (max - arr[i]);
+        }
+
+        System.out.println(ans);
+    }
+}
