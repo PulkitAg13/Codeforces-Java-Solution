fef74f782c347c7c3c3de7efc9fadece021f7e05
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-23 11:42:03 +0530

    Create Codeforces1873A.java

---
+++ Codeforces1873A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Codeforces1873A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0) {
+            String s = sc.next();
+
+            if(s.equals("abc") || 
+               s.equals("acb") || 
+               s.equals("bac") || 
+               s.equals("cba")) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
