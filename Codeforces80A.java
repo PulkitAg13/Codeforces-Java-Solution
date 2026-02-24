04c5cb185797239d47834fc5856e5eb59e262b5d
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-25 00:39:55 +0530

    Update Codeforces80A.java

--- Codeforces80A.java
+++ Codeforces80A.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Codeforces80A {
-    static boolean isPrime(int x) {
+    static boolean check(int x) {
         if (x < 2) return false;
         for (int i = 2; i * i <= x; i++) {
             if (x % i == 0) return false;
@@ -11,18 +11,23 @@ public class Codeforces80A {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        for (int i = n + 1; i <= 50; i++) {
-            if (isPrime(i)) {
-                if (i == m) {
-                    System.out.println("YES");
-                } else {
-                    System.out.println("NO");
-                }
+        int next = n + 1;
+
+        while (true) {
+            if (check(next)) {
                 break;
             }
+            next++;
+        }
+
+        if (next == m) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
     }
 }
