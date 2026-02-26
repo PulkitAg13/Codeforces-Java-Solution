1c1c225ca8e8fc350309ff0e0c616c963d605d4c
PulkitAg13 <pulkit.ag13@gmail.com>
2026-02-27 00:28:55 +0530

    Create Codeforces1374A.java

---
+++ Codeforces1374A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Codeforces1374A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0){
+            long x = sc.nextLong();
+            long y = sc.nextLong();
+            long n = sc.nextLong();
+            
+            long k = n - (n - y) % x;
+            System.out.println(k);
+        }
+    }
+}
