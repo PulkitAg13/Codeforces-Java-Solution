f3fb2f0893e31bb202b792f2ba3474f53a4a8791
PulkitAg13 <pulkit.ag13@gmail.com>
2026-03-03 00:11:27 +0530

    Create Codeforces1624A.java

---
+++ Codeforces1624A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Codeforces1624A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            
+            for(int i = 0; i < n; i++){
+                int x = sc.nextInt();
+                max = Math.max(max, x);
+                min = Math.min(min, x);
+            }
+            
+            System.out.println(max - min);
+        }
+    }
+}
