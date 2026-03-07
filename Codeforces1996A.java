01f5dff7322c023081efc22d622fe6c5fdf13037
PulkitAg13 <pulkit.ag13@gmail.com>
2026-03-07 22:18:18 +0530

    Create Codeforces1996A.java

---
+++ Codeforces1996A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Codeforces1996A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+
+            int ans = (n + 3) / 4;
+
+            System.out.println(ans);
+        }
+    }
+}
