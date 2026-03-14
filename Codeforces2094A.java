5e34a4435edddb79d19a16a16ba13a6dd1e0110d
PulkitAg13 <pulkit.ag13@gmail.com>
2026-03-14 14:55:12 +0530

    Create Codeforces2094A.java

---
+++ Codeforces2094A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Codeforces2094A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            String a = sc.next();
+            String b = sc.next();
+            String c = sc.next();
+
+            System.out.println("" + a.charAt(0) + b.charAt(0) + c.charAt(0));
+        }
+    }
+}
